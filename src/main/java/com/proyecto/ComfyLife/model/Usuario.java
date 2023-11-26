06277c57773a5b39d48dedf651976983e99c1842
Marita <maritaperezbarrio@gmail.com>
2023-11-26 12:27:07 -0500

    creacion de la estructura del proyecto y las clases model

---
+++ src/main/java/com/proyecto/ComfyLife/model/Usuario.java
@@ -0,0 +1,5 @@
+package com.proyecto.ComfyLife.model;
+
+public class Usuario {
+
+}
---
+++ src/test/java/com/proyecto/ComfyLife/SpringComfyLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.ComfyLife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringComfyLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
